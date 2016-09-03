2b0876df77a1182deeb9e7880666773b16663705
Will Ridgers <will@mindfuzz.net>
2016-09-03 17:44:50 +0100

    Add tests to eslint. Make test pass eslint. Make tests work on Windows.

--- test/integration/plugins/url-titles.js
+++ test/integration/plugins/url-titles.js
@@ -1,20 +1,23 @@
-"use strict";
-var chai = require("chai"),
-    expect = chai.expect;
+/* global describe it */
 
-describe("url-titles", function () {
-  it("should pull out a google.com url and get the correct title", function (done) {
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('url-titles', () => {
+  it('should pull out a google.com url and get the correct title', (done) => {
     var client = {
-      addListener: function (name, callback) {
-        callback("from", "to", "message containing two urls https://google.com :D");
+      addListener: (name, callback) => {
+        callback('from', 'to', 'message containing two urls https://google.com :D')
       },
       say: (to, msg) => {
-        expect(to).to.equal("to");
-        expect(msg).to.equal(">>> Google");
-        done();
+        expect(to).to.equal('to')
+        expect(msg).to.equal('>>> Google')
+        done()
       }
     }
 
     require('../../../src/plugins/url-titles.js')(client)
-  });
-});
+  })
+})
