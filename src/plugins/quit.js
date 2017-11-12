c5d503839f2d099ceb454f33d035878f9089365c
Will Ridgers <will@mindfuzz.net>
2017-11-12 15:58:14 +0000

    quit plugin

---
+++ src/plugins/quit.js
@@ -0,0 +1,14 @@
+module.exports = (client, rng) =>
+  client.addListener('message', (from, to, message) => {
+    console.log(from + ' => ' + to + ': ' + message)
+
+    if (message.indexOf('!quit') === 0) {
+      let quitMessage
+
+      if (message.indexOf(' ') > -1) {
+        quitMessage = message.substring(message.indexOf(' ') + 1)
+      }
+
+      client.disconnect(quitMessage || 'Goodbye.')
+    }
+  })
